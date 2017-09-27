a97b98865b2d17b8a3fa7afa2baabbf8408b06e4
Hallyson Ricardo <hallysonjp@gmail.com>
2017-09-26 22:39:03 -0300

    modificado o ip da API

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 /*Kartero App Configuration*/
 var krms_driver_config ={		
-	'ApiUrl':"http://localhost/kartero/api",					
+	'ApiUrl':"http://192.168.25.16/kartero/api",					
 	'DialogDefaultTitle':"O Entregador",	
 	'PushProjectID':"93275196621",	
 	'APIHasKey':""
